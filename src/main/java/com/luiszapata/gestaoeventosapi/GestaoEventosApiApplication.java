0c12182f7c879e62e7bd8c64fc2eaa102d7f1489
Luiz <luiszapataconta@gmail.com>
2026-09-10 19:43:39 -0300

    Primeiro commit

---
+++ src/main/java/com/luiszapata/gestaoeventosapi/GestaoEventosApiApplication.java
@@ -0,0 +1,13 @@
+package com.luiszapata.gestaoeventosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEventosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEventosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiszapata/gestaoeventosapi/GestaoEventosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiszapata.gestaoeventosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEventosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
