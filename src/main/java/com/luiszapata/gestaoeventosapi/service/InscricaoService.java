ea8d97b4b351a7af5797da8f79831805cfe974fa
Luis Felipe <luiszapataconta@gmail.com>
2026-09-11 08:41:08 -0300

    criacao InscricaoService com check de vaga

---
+++ src/main/java/com/luiszapata/gestaoeventosapi/service/InscricaoService.java
@@ -0,0 +1,72 @@
+package com.luiszapata.gestaoeventosapi.service;
+
+
+import com.luiszapata.gestaoeventosapi.model.Evento;
+import com.luiszapata.gestaoeventosapi.model.Inscricao;
+import com.luiszapata.gestaoeventosapi.repository.EventoRepository;
+import com.luiszapata.gestaoeventosapi.repository.InscricaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InscricaoService {
+
+
+    @Autowired
+    private InscricaoRepository inscricaoRepository;
+
+    @Autowired
+    private EventoRepository eventoRepository;
+
+    public Inscricao inscrever(Inscricao inscricao) {
+        Long eventoId = inscricao.getEvento().getId();
+
+        Optional<Evento> eventoOptional = eventoRepository.findById(eventoId);
+        Evento evento;
+
+        if (eventoOptional.isPresent()) {
+            evento = eventoOptional.get();
+        } else {
+            throw new RuntimeException("Evento não foiu encontrado com id: " + eventoId);
+        }
+
+        List<Inscricao> inscricoesDoEvento = inscricaoRepository.findByEventoId(eventoId);
+        int vagasOcupadas = inscricoesDoEvento.size();
+
+        if (vagasOcupadas >= evento.getVagasTotais()) {
+            throw new RuntimeException("Não há vagas disponíveis para este evento");
+        }
+
+        inscricao.setEvento(evento);
+        inscricao.setDataInscricao(LocalDateTime.now());
+
+        return inscricaoRepository.save(inscricao);
+
+    }
+    public List<Inscricao>listarTodas(){
+        return inscricaoRepository.findAll();
+    }
+
+    public List<Inscricao> listarPorEvento(Long eventoId) {
+        return inscricaoRepository.findByEventoId(eventoId);
+    }
+
+    public Inscricao buscarPorId(Long id) {
+        Optional<Inscricao> inscricaoOptional = inscricaoRepository.findById(id);
+
+        if (inscricaoOptional.isPresent()) {
+            return inscricaoOptional.get();
+        } else {
+            throw new RuntimeException("Inscrição não encontrada com id: " + id);
+        }
+    }
+
+    public void deletar(Long id) {
+        inscricaoRepository.deleteById(id);
+    }
+}
+
