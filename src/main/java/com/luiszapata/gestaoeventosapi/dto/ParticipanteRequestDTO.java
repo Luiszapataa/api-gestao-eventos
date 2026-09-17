027d178c22eac4fde1a8c980d2504d6ab3902ee6
Luiz <luiszapataconta@gmail.com>
2026-09-17 20:30:01 -0300

    cricao do ParticipanteRequestDTO e ParticipanteResponseDTO

---
+++ src/main/java/com/luiszapata/gestaoeventosapi/dto/ParticipanteRequestDTO.java
@@ -0,0 +1,33 @@
+package com.luiszapata.gestaoeventosapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class ParticipanteRequestDTO {
+
+    @NotBlank(message = "O nome do participante é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "O e-mail do participante é obrigatório")
+    @Email(message = "E-mail inválido")
+    private String email;
+
+    public ParticipanteRequestDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
